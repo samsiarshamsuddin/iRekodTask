cb9336ffb696cbedb2d2a2759ed0ab702fc6f11f
samsiarshamsuddin <samsiarshamsuddin@gmail.com>
2018-11-22 10:26:24 +0800

    Create NewTabWorkflowHead.js

---
+++ src/components/workflow/newActivity/NewTabWorkflowHead.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+
+const NewFolderTabHeader =({activeEditor,active,isContainer})=>{
+    
+        const sendActive=(e)=>{
+            e.preventDefault()
+            activeEditor(e.target.name)
+            console.log(e.target.name)
+            // console.log(activeEditor)
+        }
+
+    return (
+      
+<div className="row colWrap justify-content-center">
+
+    <div className="col-3 colContainer">
+    <div className={active==='newActivityWizard'?'tab activeTab mx-auto':'tab mx-auto'}>
+            <img
+            name="newActivityWizard"
+            src={require('../../../img/Group2.svg')} alt="activityImage"
+            className={active==='newActivityWizard'?'img-fluid desaturate':'img-fluid'}
+            onClick={sendActive} />
+        </div>
+    </div>
+
+    <div className={isContainer?"col-3 colContainer":"d-none"}>
+   <div className={active==='newEmail'?'tab activeTab mx-auto':'tab mx-auto'}>
+            <img
+                name="newEmail"
+                src={require('../../../img/Group2.svg')} alt="emailImage"
+                className={active==='newEmail'?'img-fluid desaturate':'img-fluid'}
+                onClick={sendActive} />
+        </div>
+    </div>
+
+   <div className={isContainer?"col-3 colContainer":"d-none"}>
+    <div className={active==='newAutoscript'?'tab activeTab mx-auto':'tab mx-auto'}>
+                <img
+                name="newAutoscript"
+                src={require('../../../img/Group2.svg')} alt="autoscriptImage"
+                className={active==='newAutoscript'?'img-fluid desaturate':'img-fluid'}
+                onClick={sendActive} />
+        </div>
+    </div> 
+
+</div>
+
+    )}
+
+
+export default NewFolderTabHeader
