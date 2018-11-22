c2fa43abe9eb34545f7d6d01d55234c553c23bce
samsiarshamsuddin <samsiarshamsuddin@gmail.com>
2018-11-22 10:25:30 +0800

    Update types.js

--- src/actions/types.js
+++ src/actions/types.js
@@ -9,7 +9,43 @@ export const PAGE_CLASS = 'PAGE_CLASS'
 export const SIDENAV_CLASS = 'SIDENAV_CLASS'
 export const TOGGLE_SIDENAV = 'TOGGLE_SIDENAV'
 export const ACTIVE_PAGE = 'ACTIVE_PAGE'
-
+export const PAGE_TITLE = 'PAGE_TITLE'
 
 //search action and reducer link
 export const BASIC_SEARCH = 'BASIC_SEARCH'
+
+//list workflow action and reducer link
+export const LIST_WORKFLOW = 'LIST_WORKFLOW'
+export const LIST_OF_SUBJECT = 'LIST_OF_SUBJECT'
+export const LIST_OF_CHILDSUBJECT = 'LIST_OF_CHILDSUBJECT'
+
+//breadcrumb action and reducer link
+export const SET_BREADCRUMB = 'SET_BREADCRUMB'
+export const INSERT_NEW_BREADCRUMB = 'INSERT_NEW_BREADCRUMB'
+
+//listworkflow action and reducer link
+export const SET_CARD_VIEW = 'SET_CARD_VIEW'
+export const CHANGE_ISMULTI = 'CHANGE_ISMULTI'
+export const SET_SEL_ALL = 'SET_SEL_ALL'
+export const WORKFLOW_SEL = 'WORKFLOW_SEL'
+export const SHOW_FAB = 'SHOW_FAB'
+export const SELECT_SEL = 'SELECT_SEL'
+
+//listworkflowdetails action and reducer link
+export const LIST_ACTIVITY = 'SET_ACTIVITY'
+export const LIST_EMAIL = 'LIST_EMAIL'
+export const WIZARD_PAGE = 'WIZARD_PAGE'
+export const SET_ACTIVITY_STORE = 'SET_ACTIVITY_STORE'
+export const SET_EMAIL_STORE = 'SET_EMAIL_STORE'
+export const LIST_ACTIVITY_DETAIL = 'LIST_ACTIVITY_DETAIL'
+export const LIST_STAKEHOLDER = 'LIST_STAKEHOLDER'
+export const LIST_SUBJECT_ITEM = 'LIST_SUBJECT_ITEM'
+export const SET_CONTAINER_LINE = 'SET_CONTAINER_LINE'
+export const DELETE_WORKFLOW = 'DELETE_WORKFLOW'
+
+//create new activity action and reducer link
+export const WIZARD_PAGE_ADD = 'WIZARD_PAGE_ADD'
+export const SET_CONTAINER_LINE_ADD = 'SET_CONTAINER_LINE_ADD'
+export const LIST_STAKEH_ADD = 'LIST_STAKEH_ADD'
+export const LIST_WORKFLOWBY_SUBJECT = 'LIST_WORKFLOWBY_SUBJECT'
+export const LIST_EMAIL_ADD = 'LIST_EMAIL_ADD'
