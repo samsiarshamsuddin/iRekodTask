74571e26eaf8c8fb7771271c3b298c5cdcad272a
samsiarshamsuddin <samsiarshamsuddin@gmail.com>
2018-11-22 10:25:20 +0800

    Create createNewActAction.js

---
+++ src/actions/createNewActAction.js
@@ -0,0 +1,68 @@
+import {WIZARD_PAGE_ADD, SET_CONTAINER_LINE_ADD, LIST_STAKEH_ADD, LIST_WORKFLOWBY_SUBJECT, LIST_EMAIL_ADD} from './types'
+
+import {biorisUrl} from '../config/appConf'
+
+
+export const setWizardPageNew=(param)=>{
+    return {
+        type:WIZARD_PAGE_ADD,
+        payload:param
+    }
+}
+
+export const setContinerLineNew=(param)=>{
+    return {
+        type:SET_CONTAINER_LINE_ADD,
+        payload:param
+    }
+}
+
+export const setStakehListNew=(stakehList)=>dispatch=>{
+    const url=`${biorisUrl}/stakeholder?param=${JSON.stringify(stakehList)}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(res=>{
+        dispatch({
+            type: LIST_STAKEH_ADD,
+            payload: res.results
+            })
+            })
+
+}
+
+export const setItemListSubject=(listWorflowbySub)=>dispatch=>{
+    const url=`${biorisUrl}/tasks?param=${JSON.stringify(listWorflowbySub)}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(res=>{
+        console.log(res)
+        dispatch({
+            type: LIST_WORKFLOWBY_SUBJECT,
+            payload: res.results
+            })
+            })
+
+}
+
+
+export const setEmailStoreNew = (param) => dispatch =>{
+    // console.log(param)
+    const url=`${biorisUrl}/emailTemplate?param=${JSON.stringify(param)}`
+        fetch(url,{method:'GET'})
+        .then(res=>res.json())
+        .then(res=>{
+            // console.log(res)
+            dispatch({
+                type:LIST_EMAIL_ADD,
+                payload:res.results
+            })
+        })
+}
+
+
+
+
+ 
+
+
+
