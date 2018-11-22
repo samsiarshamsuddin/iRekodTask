5e7ba9d6c4d4b63734ae7ffbc64ccf1a47bad88b
samsiarshamsuddin <samsiarshamsuddin@gmail.com>
2018-11-22 10:25:36 +0800

    Create DashPage.js

---
+++ src/components/dashboard/DashPage.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+
+export default class DashPage extends Component {
+
+  render() {
+      return(
+        <Fragment>
+        <div>DASHBOARD</div>
+      </Fragment>
+
+      )
+  }
+}
