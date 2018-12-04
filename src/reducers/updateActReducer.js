f46d04cd56e300fcdd501cf845761c247639e90e
samsiarshamsuddin <samsiarshamsuddin@gmail.com>
2018-12-04 17:04:34 +0800

    Create updateActReducer.js

---
+++ src/reducers/updateActReducer.js
@@ -0,0 +1,25 @@
+import {UPDATE_ACTIVITY, LIST_UPDATE_ACTIVITY} from '../actions/types'
+
+const initialState={
+    updAct:[],
+    activityDet : []
+}
+
+export default function(state = initialState, action){
+    switch(action.type){ 
+        case UPDATE_ACTIVITY:
+        return {
+            ...state,
+            updAct:action.payload,
+        }
+
+         case LIST_UPDATE_ACTIVITY:
+        return {
+            ...state,
+            activityDet:action.payload,
+        }
+
+        default:
+        return state
+    }
+}
