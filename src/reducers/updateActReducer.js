e7aa83c0571c9a6c2655d7ee90f21e12a8e61ea0
samsiarshamsuddin <samsiarshamsuddin@gmail.com>
2018-12-06 16:44:10 +0800

    Update updateActReducer.js

--- src/reducers/updateActReducer.js
+++ src/reducers/updateActReducer.js
@@ -1,8 +1,11 @@
-import {UPDATE_ACTIVITY, LIST_UPDATE_ACTIVITY} from '../actions/types'
+import {UPDATE_ACTIVITY, LIST_UPDATE_ACTIVITY, SELECTED_RECIPIENTS, LIST_EMAIL_RECIPIENTS, SELECTED_INC_STAKEH} from '../actions/types'
 
 const initialState={
     updAct:[],
     activityDet : [],
+    recipients: [], 
+    emailRecipients:[],
+    incStakehObj:[]
 }
 
 export default function(state = initialState, action){
@@ -17,6 +20,23 @@ export default function(state = initialState, action){
         return {
             ...state,
             activityDet:action.payload,
+        }
+
+        case SELECTED_RECIPIENTS:
+        return {
+            ...state,
+            recipients:action.payload,
+        }
+
+        case LIST_EMAIL_RECIPIENTS:
+        return {
+            ...state,
+            emailRecipients:action.payload,
+        }
+        case SELECTED_INC_STAKEH:
+        return {
+            ...state,
+            incStakehObj:action.payload,
         }
 
         default:
