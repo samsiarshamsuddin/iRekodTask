49bffc6c567d808eb1e48744a82558b79a796b0a
samsiarshamsuddin <samsiarshamsuddin@gmail.com>
2018-12-06 16:43:42 +0800

    Update updateActAction.js

--- src/actions/updateActAction.js
+++ src/actions/updateActAction.js
@@ -1,4 +1,4 @@
-import {UPDATE_ACTIVITY, LIST_UPDATE_ACTIVITY} from './types'
+import {UPDATE_ACTIVITY, LIST_UPDATE_ACTIVITY, SELECTED_RECIPIENTS, LIST_EMAIL_RECIPIENTS, SELECTED_INC_STAKEH} from './types'
 
 import {biorisUrl} from '../config/appConf'
 
@@ -27,4 +27,24 @@ export const setActivityDetailsUpdate=(activityDet)=>dispatch=>{
 
 }
 
+export const setRecipients=(param)=>{
+    return {
+        type:SELECTED_RECIPIENTS,
+        payload:param
+    }
+}
+
+export const taskEmailRecipients=(param)=>{
+    return {
+        type:LIST_EMAIL_RECIPIENTS,
+        payload:param
+    }
+}
+
+export const setIncStakeh=(param)=>{
+    return {
+        type:SELECTED_INC_STAKEH,
+        payload:param
+    }
+}
 
